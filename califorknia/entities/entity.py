1a6701a5bb55825fe08b3beae9c98af531a0659e
Bratah123 <brandonnguyen3301@gmail.com>
2022-09-30 22:32:15 -0700

    CHORE: Add game objects
    Creates a basis of classes

---
+++ califorknia/entities/entity.py
@@ -0,0 +1,8 @@
+"""
+    This represents any humanoid in the game, Aliens, Players, and NPCs.
+    Player and NPCs will inherit from this class.
+"""
+
+
+class Entity:
+    pass
---
+++ califorknia/entities/player.py
@@ -0,0 +1,5 @@
+from califorknia.entities.entity import Entity
+
+
+class Player(Entity):
+    pass
